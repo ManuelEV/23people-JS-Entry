b204f7ce24fbc3eb8d3966ec0fdfc7c955c99002
Manuel <manuelespv@gmail.com>
2020-09-07 17:46:54 -0300

    fix input button test file

--- calculator/src/__tests__/InputButton.test.js
+++ calculator/src/__tests__/InputButton.test.js
@@ -1,40 +1,4 @@
 import React from "react";
-<<<<<<< Updated upstream
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-
-import InputButton from '../components/buttons/InputButton';
-
-
-let container = null;
-
-beforeEach(() => {
-  // set up a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("renders without crashing: Button", () => {
-    render(<InputButton input="1" value="1" onClick={() => function test() {}} />, container);
-})
-
-it("check button content", () => {
-
-  act(() => {
-    render(<InputButton input="1" value="1" onClick={() => function test() {}} />, container);
-  });
-  expect(container.textContent).toBe("1");
-  
-});
-
-=======
 import { render } from "react-dom";
 import InputButton from '../components/buttons/InputButton';
 
@@ -46,4 +10,3 @@ it("renders without crashing: Button", () => {
     render(<InputButton input="1" value="1" onClick={() => function test() {}} />, div);
     
 })
->>>>>>> Stashed changes
